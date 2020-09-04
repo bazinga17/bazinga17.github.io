71c79b059d3bcd4d1fbd65644ee380d8446b1c9f
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-04 14:48:19 +0300

    78545645

--- js/section.js
+++ js/section.js
@@ -5,7 +5,7 @@ var time_spend = document.getElementById('get_time').value;
 var _date = document.getElementById('date_in_time').value;
 
 document.getElementById('add_work').addEventListener('click', function () {
-    console.log(t.card());
+    console.log(t.card().iframe);
 
     //var x = document.getElementById("list_date");
     //var opt = document.createElement("li");
