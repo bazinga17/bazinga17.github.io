575a1655bf4b29f25cd756a459912ae9c1146ea1
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-07 15:30:30 +0300

    32487854521

--- js/section.js
+++ js/section.js
@@ -4,9 +4,10 @@ var t = TrelloPowerUp.iframe();
 var time_spend = document.getElementById('get_time').value;
 var _date = document.getElementById('date_in_time').value;
 
-//t.render(function () {
-//    return Promise.all([t.get('card', 'shared', 'time_list')]);
-//});
+t.render(function () {
+    //return Promise.all([t.get('card', 'shared', 'time_list')]);
+    document.getElementById('date_in_time').value = Date.now;
+});
 
 document.getElementById('add_work').addEventListener('click', function () {    
     t.set('card', 'shared', 'time_list', time_spend + _date);
