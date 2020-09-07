cde7b4e0804536ca5ef52f19c0ba5de37fd27081
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-07 14:23:31 +0300

    1248787

--- js/worklogs.js
+++ js/worklogs.js
@@ -1,11 +1,11 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-return t.get('card', 'shared', 'time_list').then(function (data) { JSON.stringify(data, null, 2)});
+//return t.get('card', 'shared', 'time_list').then(function (data) { JSON.stringify(data, null, 2)});
 
-var x = document.getElementById('list_date');
-var opt = document.createElement('li');
-opt.text = data;
-console.log(x);
+//var x = document.getElementById('list_date');
+//var opt = document.createElement('li');
+//opt.text = data;
+//console.log(x);
 
-console.log(data);
+//console.log(data);
