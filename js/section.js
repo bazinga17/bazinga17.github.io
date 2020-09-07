0bec8bea5c9497ca0eb8cf3335c09f930eea497a
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-07 13:27:39 +0300

    set ge4t

--- js/section.js
+++ js/section.js
@@ -4,14 +4,10 @@ var t = TrelloPowerUp.iframe();
 var time_spend = document.getElementById('get_time').value;
 var _date = document.getElementById('date_in_time').value;
 
+//t.render(function () {
+//    return Promise.all([t.get('card', 'shared', 'time_list')]);
+//});
+
 document.getElementById('add_work').addEventListener('click', function () {
-    console.log(t.card().iframe);
-
-    //var x = document.getElementById("list_date");
-    //var opt = document.createElement("li");
-    //opt.text = time_spend + ' ' + _date;
-
-    //console.log(x);
-
-    //return console.log(time_spend + _date)
+    return t.set('card', 'shared', 'time_list', time_spend + _date);
 })
--- js/worklogs.js
+++ js/worklogs.js
@@ -1,1 +1,11 @@
+var Promise = TrelloPowerUp.Promise;
+var t = TrelloPowerUp.iframe();
 
+return t.get('card', 'shared', 'time_list').then(function (data) { });
+
+var x = document.getElementById("list_date");
+var opt = document.createElement("li");
+opt.text = data;
+console.log(x);
+
+console.log(data);
