e4a13147d133a9453932c3e74b24f90030b9a8b1
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-08 15:57:42 +0300

    fsdfssdf

--- js/section.js
+++ js/section.js
@@ -5,23 +5,23 @@ var time_spend = document.getElementById('get_time').value;
 var _date = document.getElementById('date_in_time').value;
 
 
-window.insert_time.addEventListener('submit', function (event) {
-    event.preventDefault();
-    console.log(time_spend);
-    return t.set('card', 'shared', 'time_list', time_spend).then(function () { t.closePopup(); });
-}); 
+//window.insert_time.addEventListener('submit', function (event) {
+//    event.preventDefault();
+//    console.log(time_spend);
+//    return t.set('card', 'shared', 'time_list', time_spend).then(function () { t.closePopup(); });
+//}); 
 
-t.render(function () {
-    return t.getAll()//('card', 'shared', 'time_list')
-        //.then(function (time_list) {
-        //    window.get_time.value = time_list;
-        //    console.log(time_list);
-        //})
-        .then(function (data) {
-            console.log(JSON.stringify(data));
-            t.sizeTo('#insert_time').done()
-        });
-});
+//t.render(function () {
+//    return t.getAll()//('card', 'shared', 'time_list')
+//        //.then(function (time_list) {
+//        //    window.get_time.value = time_list;
+//        //    console.log(time_list);
+//        //})
+//        .then(function (data) {
+//            console.log(JSON.stringify(data));
+//            t.sizeTo('#insert_time').done()
+//        });
+//});
 
 
 //t.render(function () {
@@ -33,13 +33,13 @@ t.render(function () {
 
 //});
 
-//document.getElementById('add_work').addEventListener('click', function () {    
-//    t.set('card', 'shared', 'time_list', time_spend + _date);
+document.getElementById('add_work').addEventListener('click', function () {    
+    t.set('card', 'shared', 'time_list', time_spend + _date);
 
-//    t.get('card', 'shared', 'time_list', 'not set')
-//        .then(function (data) {
-//            console.log(JSON.stringify(data, null, 2));
-//        });
+    t.get('card', 'shared', 'time_list', 'not set')
+        .then(function (data) {
+            console.log(JSON.stringify(data, null, 2));
+        });
 
-//    return console.log('add');
-//})
+    return console.log(time_spend + _date);
+})
