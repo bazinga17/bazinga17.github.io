daed93b08a938dfdd87184755c0f59eb2f6f3d34
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-08 16:23:25 +0300

    fsdf;' e4r

--- js/section.js
+++ js/section.js
@@ -12,11 +12,11 @@ window.insert_time.addEventListener('submit', function (event) {
 }); 
 
 t.render(function () {
-    return t.getAll()//('card', 'shared', 'time_list')
-        //.then(function (time_list) {
-        //    window.get_time.value = time_list;
-        //    console.log(time_list);
-        //})
+    return t.get('card', 'shared', 'time_list')
+        .then(function (time_list) {
+            window.get_time.value = time_list;
+            console.log(time_list);
+        })
         .then(function (data) {
             console.log(JSON.stringify(data));
             t.sizeTo('#insert_time').done()
