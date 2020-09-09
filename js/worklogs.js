a8ba2b01ef90f26649888bdd07db15404053a008
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-09 13:38:09 +0300

    12487 4

--- js/worklogs.js
+++ js/worklogs.js
@@ -5,9 +5,10 @@ t.render(function () {
     return t.get('card', 'shared', 'time_list', 'not set')
         .then(function (data) {
             var x = document.getElementById('list_date');
+            console.log(x);
             var opt = document.createElement('li');
             opt.text = data;
-            x.appendChild(opt);
+            x.append(opt);
 
         }).then(function () {
             t.sizeTo('#content');
