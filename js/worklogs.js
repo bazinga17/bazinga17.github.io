9ffd778bf0f05136fb4ed8b6265f3713c704818d
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-09 14:03:45 +0300

    wl

--- js/worklogs.js
+++ js/worklogs.js
@@ -5,11 +5,10 @@ t.render(function () {
     return t.get('card', 'shared', 'time_list', 'not set')
         .then(function (data) {
             var x = document.getElementById('list_date');
-            console.log(x);
             var opt = document.createElement('li');
-            opt.text = data;
-            x.append(opt);
-
+            var node = document.createTextNode(data);
+            opt.appendChild(node);
+            x.appendChild(opt);
         }).then(function () {
             t.sizeTo('#content');
         });
