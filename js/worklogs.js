7095c32926a8e6a66ddb89c52bc69278da29f5ed
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-09 13:17:49 +0300

    vxcv erwe

--- js/worklogs.js
+++ js/worklogs.js
@@ -1,14 +1,16 @@
 var Promise = TrelloPowerUp.Promise;
 var t = TrelloPowerUp.iframe();
 
-
-t.get('card', 'shared', 'time_list', 'not set')
-    .then(function (data) {
-        var x = document.getElementById('list_date');
-        var opt = document.createElement('li');
-        opt.text = data;
-        console.log(data);
-    });
+t.render(function () {
+    return t.get('card', 'shared', 'time_list', 'not set')
+        .then(function (data) {
+            var x = document.getElementById('list_date');
+            var opt = document.createElement('li');
+            opt.text = data;
+        }).then(function () {
+            t.sizeTo('#content');
+        });
+});
 
 
 
