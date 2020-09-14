874d3cc9f9bed94f072c8372bcce34130af4fc5a
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-14 14:26:57 +0300

    list time 4

--- js/section.js
+++ js/section.js
@@ -102,13 +102,14 @@ window.insert_time.addEventListener('submit', function (event) {
         //}
         //else {
             //let list_us = [{ name: 'user', time_start: _date.value, spend: time_spend.value }];
-            let list_us = [{ name: 'user', time_start: '0', spend: '0' }];
+        let list_us = [{ hour: 0, min: 0, sec: 0 }];
+            //let list_us = [{ name: 'user', time_start: '0', spend: '0' }];
             //list_us[0].name = 'user';
             //list_us[0].time_start = _date.value;
             //list_us[0].spend = time_spend.value;
 
             console.log(list_us);
-            return t.set('card', 'shared', 'user_list', list_us).then(function () { t.closePopup(); });
+            //return t.set('card', 'shared', 'user_list', list_us).then(function () { t.closePopup(); });
         //}
     });
 }); 
