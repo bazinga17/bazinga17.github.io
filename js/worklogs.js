b5058df3a50ea9789002f22244e5aa4685aea527
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-15 09:29:52 +0300

    time list 9

--- js/worklogs.js
+++ js/worklogs.js
@@ -4,18 +4,17 @@ var t = TrelloPowerUp.iframe();
 t.render(function () {
     return t.get('card', 'shared', 'user_list', 'not set')
         .then(function (time_list) {
-            console.log(JSON.stringify(time_list));
-            //for (var i = 0; i < time_list.length; i++) {
+            for (var i = 0; i < time_list.length; i++) {
 
-            //    var x = document.getElementById('list_date');
-            //    var opt = document.createElement('li');
-            //    var node = document.createTextNode(time_list[i].name + ' - ' + time_list[i].time_start + ' ' + time_list[i].time_spend);
-            //    opt.appendChild(node);
-            //    x.appendChild(opt);
-            //}
-        })//.then(function () {
-        //    t.sizeTo('#content');
-        //});
+                var x = document.getElementById('list_date');
+                var opt = document.createElement('li');
+                var node = document.createTextNode(time_list[i].name + ' - ' + time_list[i].time_start + ' ' + time_list[i].time_spend);
+                opt.appendChild(node);
+                x.appendChild(opt);
+            }
+        }).then(function () {
+            t.sizeTo('#content');
+        });
 });
 
 
