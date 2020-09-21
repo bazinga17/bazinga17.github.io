faa92649bd1da2cb1420215e288884a9b53fc6dd
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-21 16:38:43 +0300

    unify 24

--- js/section.js
+++ js/section.js
@@ -87,10 +87,9 @@ window.insert_time.addEventListener('submit', function (event) {
                     });
                     
                     t.set('card', 'shared', 'user_list', users);
-
-                    get_spend(time_spend.value, date_time);
-                    return t.set('card', 'shared', 'all_time', date_time);
                 });
+                get_spend(time_spend.value, date_time);
+                return t.set('card', 'shared', 'all_time', date_time);
             }
         }
     }).then(function () { t.closePopup() });
