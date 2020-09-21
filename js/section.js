4e8c46e31129bcbd24b6a803c97a662d7d021976
boldyreva_gg <boldyreva_gg@LG-D-666D8D1494>
2020-09-21 13:11:21 +0300

    unify 1

--- js/section.js
+++ js/section.js
@@ -10,106 +10,139 @@ function div(val) {
     return (val - val % 60) / 60;
 }
 
+function get_spend(interval, d_val) {
+    let res = interval.value.split(" ", 3);
+
+    if (res.length == 3) // all time
+    {
+        let _sec = parseFloat(res[2]) + d_val[0].sec;
+        let _min = parseFloat(res[1]) + d_val[0].min;
+
+        var s_del = div(_sec);
+        var m_del = div(_min);
+
+        d_val[0].sec = _sec - 60 * s_del;
+        d_val[0].min = _min - 60 * m_del + s_del;
+        d_val[0].hour += parseFloat(res[0]) + m_del;
+    }
+    else if (res.length == 2)// h and min
+    {
+        let _min = parseFloat(res[1]) + d_val[0].min;
+
+        var m_del = div(_min);
+
+        d_val[0].min = _min - 60 * m_del;
+        d_val[0].hour += parseFloat(res[0]) + m_del;
+    }
+    else if (res.length == 1) //h
+    {
+        d_val[0].hour += parseFloat(res[0]);
+    }
+
+    return d_val;
+}
+
 window.insert_time.addEventListener('submit', function (event) {
     event.preventDefault();
 
-    //t.get('card', 'shared', 'all_time', 'no')
-    //    .then(function (all_time) {
+    t.get('card', 'shared', 'all_time', 'no')
+        .then(function (all_time) {
 
-    //        if (all_time != 'no')
-    //        {
-    //            let date_time = all_time;//for all time
+            if (all_time != 'no')
+            {
+                let date_time = all_time;//for all time
 
-    //            console.log(date_time);
+                if (time_spend.value != "")
+                {
+                    var res = time_spend.value.split(" ", 3);
 
-    //            if (time_spend.value != "")
-    //            {
-    //                var res = time_spend.value.split(" ", 3);
+                    //if (res.length == 3) // all time
+                    //{
+                    //    let _sec = parseFloat(res[2]) + date_time[0].sec;
+                    //    let _min = parseFloat(res[1]) + date_time[0].min;
 
-    //                if (res.length == 3) // all time
-    //                {
-    //                    let _sec = parseFloat(res[2]) + date_time[0].sec;
-    //                    let _min = parseFloat(res[1]) + date_time[0].min;
+                    //    var s_del = div(_sec);
+                    //    var m_del = div(_min);
 
-    //                    var s_del = div(_sec);
-    //                    var m_del = div(_min);
+                    //    date_time[0].sec = _sec - 60 * s_del;
+                    //    date_time[0].min = _min - 60 * m_del + s_del;
+                    //    date_time[0].hour += parseFloat(res[0]) + m_del;
 
-    //                    date_time[0].sec = _sec - 60 * s_del;
-    //                    date_time[0].min = _min - 60 * m_del + s_del;
-    //                    date_time[0].hour += parseFloat(res[0]) + m_del;
+                    //}
+                    //else if (res.length == 2)// h and min
+                    //{
+                    //    let _min = parseFloat(res[1]) + date_time[0].min;
 
-    //                }
-    //                else if (res.length == 2)// h and min
-    //                {
-    //                    let _min = parseFloat(res[1]) + date_time[0].min;
+                    //    var m_del = div(_min);
 
-    //                    var m_del = div(_min);
+                    //    date_time[0].min = _min - 60 * m_del;
+                    //    date_time[0].hour += parseFloat(res[0]) + m_del;
+                    //}
+                    //else if (res.length== 1) //h
+                    //{
+                    //    date_time[0].hour += parseFloat(res[0]);
+                    //}
+                }
+                console.log(get_spend(time_spend.value, date_time));
 
-    //                    date_time[0].min = _min - 60 * m_del;
-    //                    date_time[0].hour += parseFloat(res[0]) + m_del;
-    //                }
-    //                else if (res.length== 1) //h
-    //                {
-    //                    date_time[0].hour += parseFloat(res[0]);
-    //                }
-    //            }
-    //            console.log(date_time);
-    //            return t.set('card', 'shared', 'all_time', date_time).then(function () { t.closePopup(); });
-    //        }
-    //        else                //new card
-    //        {
-    //            let date_time = [{ hour: 0, min: 0, sec: 0 }];//for all time
+                //return t.set('card', 'shared', 'all_time', date_time).then(function () { t.closePopup(); });
+            }
+            else                //new card
+            {
+                let date_time = [{ hour: 0, min: 0, sec: 0 }];//for all time
 
-    //            if (time_spend.value != "")
-    //            {                    
-    //                var res = time_spend.value.split(" ", 3);
+                if (time_spend.value != "")
+                {                    
+                    var res = time_spend.value.split(" ", 3);
 
-    //                if (res.length == 3) // all time
-    //                {
-    //                    var s_del = div(parseFloat(res[2]));
-    //                    var m_del = div(parseFloat(res[1]));
+                    if (res.length == 3) // all time
+                    {
+                        var s_del = div(parseFloat(res[2]));
+                        var m_del = div(parseFloat(res[1]));
 
-    //                    date_time[0].sec = parseFloat(res[2]) - 60 * s_del;
-    //                    date_time[0].min = parseFloat(res[1]) - 60 * m_del + s_del;
-    //                    date_time[0].hour = parseFloat(res[0]) + m_del;
+                        date_time[0].sec = parseFloat(res[2]) - 60 * s_del;
+                        date_time[0].min = parseFloat(res[1]) - 60 * m_del + s_del;
+                        date_time[0].hour = parseFloat(res[0]) + m_del;
 
-    //                }
-    //                else if (res.length == 2)// h and min
-    //                {
-    //                    var m_del = div(parseFloat(res[1]));
+                    }
+                    else if (res.length == 2)// h and min
+                    {
+                        var m_del = div(parseFloat(res[1]));
 
-    //                    date_time[0].min = parseFloat(res[1]) - 60 * m_del;
-    //                    date_time[0].hour = parseFloat(res[0]) + m_del;
-    //                }
-    //                else if (res.length == 1) //h
-    //                {
-    //                    date_time[0].hour = parseFloat(res[0]);
-    //                }
+                        date_time[0].min = parseFloat(res[1]) - 60 * m_del;
+                        date_time[0].hour = parseFloat(res[0]) + m_del;
+                    }
+                    else if (res.length == 1) //h
+                    {
+                        date_time[0].hour = parseFloat(res[0]);
+                    }
 
-    //                return t.set('card', 'shared', 'all_time', date_time).then(function () { t.closePopup(); });
-    //            }
-    //        }
-    //    });
+                    return t.set('card', 'shared', 'all_time', date_time).then(function () { t.closePopup(); });
+                }
+            }
+        });
 
     t.get('card', 'shared', 'user_list123', 'no').then(function (list_user)
     {
         if (list_user != 'no') {
             let users = list_user;
 
-            users.push({ name: 'name1', time_start: _date.value, spend: time_spend.value });
+            let val = new Date(_date.value);
+            let _d = val.getFullYear() + '/' + (val.getMonth() + 1) + '/' + val.getDate();
+            let _t = val.getHours() + ':' + val.getMinutes() + ':' + val.getSeconds();
+
+            users.push({ name: 'name1', date_in: _d, time_in: _t, spend: time_spend.value });
 
             return t.set('card', 'shared', 'user_list', users).then(function () { t.closePopup(); });
         }
         else {
             let users = [];
+
             let val = new Date(_date.value);
             let _d = val.getFullYear() + '/' + (val.getMonth() + 1) + '/' + val.getDate();
             let _t = val.getHours() + ':' + val.getMinutes() + ':' + val.getSeconds();
 
-            console.log(_d);
-            console.log(_t);
-
-            users.push({ name: 'name1', date_in: _date.value, spend: time_spend.value });
+            users.push({ name: 'name1', date_in: _d, time_in: _t, spend: time_spend.value });
 
             return t.set('card', 'shared', 'user_list', users).then(function () { t.closePopup(); });
         }
